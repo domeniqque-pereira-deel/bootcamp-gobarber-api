bba8b85bba30daa0ff2635eec11ef38cc0d89445
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-08-20 17:27:17 -0400

    chore: config sequelize database

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
