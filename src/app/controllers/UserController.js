4298d77462069ae16b0ed2e60c9133a4a67f5c8d
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-08-20 18:35:40 -0400

    feat: create user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,19 @@
+import Users from '../models/Users';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await Users.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email, provider } = await Users.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
