39c88f169341519dc31f80ae58c56e2f3000b5f7
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-08-23 05:31:55 -0400

    feat: list providers and add appointments

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
+import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-
-import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 
@@ -19,6 +20,10 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
