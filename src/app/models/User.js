aa8a9afc648ecb3bc4ad08f808af1c380e8a966e
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-08-23 05:31:19 -0400

    chore: serve static files

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -9,7 +9,7 @@ class User extends Model {
         email: Sequelize.STRING,
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
-        provider: Sequelize.DATE,
+        provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
@@ -26,7 +26,7 @@ class User extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
   }
 
   checkPassword(password) {
