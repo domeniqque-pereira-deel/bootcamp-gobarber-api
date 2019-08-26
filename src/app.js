3d8f9a67a433bb2313b396f75a64db4aa5dc527a
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-08-26 18:40:06 -0400

    feat: add exception handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import { resolve } from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import sentryConfig from './config/sentry';
+
+import 'express-async-errors';
 import routes from './routes';
 
 import './database';
@@ -8,20 +13,33 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(
       '/files',
       express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
     );
   }
 
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
+  }
+
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://0301b404fb6644ff86f1b23aad0b957c@sentry.io/1542402',
+};
