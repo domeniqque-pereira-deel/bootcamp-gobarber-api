4c233a07649fd3e2ed0b5debe838dfdfd43604f1
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-10-05 15:27:15 -0400

    chore: add cors and return user avatar

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { resolve } from 'path';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
@@ -25,6 +26,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
 
     if (isProduction) {
       this.server.use(Sentry.Handlers.requestHandler());
